d0cd17d3a93cdf30ef1271047f908166c4e60f36
Ooblioob <Ooblioob@gmail.com>
2016-03-19 15:04:40 +0000

    Tweaking test setup, adding mocha

--- vendor/gems/imdb_consume/test/test_helper.rb
+++ vendor/gems/imdb_consume/test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'imdb_consume'
 
 require 'minitest/autorun'
+require 'mocha/mini_test'
+require 'json' # converting objects to and from json
+require 'active_support/all' # need .present?
